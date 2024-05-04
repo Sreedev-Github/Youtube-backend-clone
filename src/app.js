4dc0f19a66ccef0e2e571e5828291f8b473af18a
Sreedev-Github <sreedev0612@gmail.com>
2024-05-04 20:45:18 +0530

    set up basic route and controller for it

--- src/app.js
+++ src/app.js
@@ -19,4 +19,16 @@ app.use(cookieParser());
 // Security practices with middleware
 app.use(express.json({ limit: "16kb" }));
 
+
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:3000/api/v1/users/register
+
 export { app };
